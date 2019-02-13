8ff52e8a50854157db710c0e4f413427ffcf8422
lakshay91 <lakshay91dutt@gmail.com>
2019-02-13 15:07:56 +0530

    Added search panel + removable users

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
 import ListContacts from './components/list-contacts'
-const contacts = [
-  {
-    'id': 'lakshay',
-    'name': 'Lakshay Dutt',
-    'email': 'lakshay91@gmail.com',
-    'avatarUrl': 'https://cdn.iconscout.com/icon/free/png-256/account-profile-avatar-man-circle-round-user-30452.png'
-  },
-  {
-    'id': 'lawliet',
-    'name': 'L Lawliet',
-    'email': 'l@gmail.com',
-    'avatarUrl': 'https://cdn4.iconfinder.com/data/icons/mask/512/death-dead-batman-head-face-round-avatar-512.png'
-  },
-  {
-    'id': 'kira',
-    'name': 'Light',
-    'email': 'light@gmail.com',
-    'avatarUrl': 'https://cdn2.iconfinder.com/data/icons/super-hero/154/batman-comics-hero-avatar-head-mask-512.png'
-  }
-];
+
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        'id': 'lakshay',
+        'name': 'Lakshay Dutt',
+        'email': 'lakshay91@gmail.com',
+        'avatarUrl': 'https://cdn.iconscout.com/icon/free/png-256/account-profile-avatar-man-circle-round-user-30452.png'
+      },
+      {
+        'id': 'lawliet',
+        'name': 'L Lawliet',
+        'email': 'l@gmail.com',
+        'avatarUrl': 'https://cdn4.iconfinder.com/data/icons/mask/512/death-dead-batman-head-face-round-avatar-512.png'
+      },
+      {
+        'id': 'kira',
+        'name': 'Light',
+        'email': 'light@gmail.com',
+        'avatarUrl': 'https://cdn2.iconfinder.com/data/icons/super-hero/154/batman-comics-hero-avatar-head-mask-512.png'
+      },
+      {
+        'id': 'manish',
+        'name': 'Manish Jain',
+        'email': 'manishJain@gmail.com',
+        'avatarUrl': 'http://anderworx.com/wp-content/uploads/2018/09/cropped-Avatar-Round.png'
+      }
+    ]
+  }
+
+  removeContact = (delContact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter(element => element.id !== delContact.id)
+    }))
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts}></ListContacts>
+        <ListContacts
+          contacts={this.state.contacts}
+          handleRemoveUser={this.removeContact}>
+        </ListContacts>
       </div>
     );
   }
--- src/components/list-contacts.js
+++ src/components/list-contacts.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        handleRemoveUser: PropTypes.func.isRequired
+    }
 
-const ListContacts = (props) => {
-    return (
-        <ol className='contact-list'>
-            {props.contacts.map((contact, index) => (
-                <li key={index} className='contact-list-item'>
-                    {/* Avatar */}
-                    <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarUrl})` }}></div>
-                    {/* Details of the Avatar */}
-                    <div className= 'contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    {/* Delete user button */}
-                    <button className = 'contact-remove'></button>
-                </li>
-            ))}
-        </ol>
-    );
+    state = {
+        query: ''
+    }
+
+    handleQueryChange = (newQuery) => {
+        this.setState({
+            query: newQuery
+        })
+        
+    }
+    render() {
+        return (
+            <div className='list-contacts'>
+                
+                {/* Top Panel -> Contains search box and Add user button*/}
+                <div className='list-contacts-top'>
+                    {/* Search Panel */}
+                    <input
+                        type='text' placeholder='Search Employee'
+                        className='search-contacts' value={this.state.query}
+                        onChange={(event) => this.handleQueryChange(event.target.value)}>
+                    </input>
+                    {/* Add user */}
+                    <div className='add-contact'></div>
+                </div>
+
+                <ol className='contact-list'>
+                    {this.props.contacts.map((contact, index) => (
+                        <li key={index} className='contact-list-item'>
+
+                            {/* Avatar */}
+                            <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarUrl})` }}></div>
+
+                            {/* Details of the Avatar */}
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+
+                            {/* Delete user button */}
+                            <button className='contact-remove' onClick={() => this.props.handleRemoveUser(contact)}></button>
+
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        );
+    }
 }
 export default ListContacts;
